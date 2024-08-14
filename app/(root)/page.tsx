ebad074346dd9eb59d86419799c2a8359e6af116
Epris Richardson <33326194+eprisr@users.noreply.github.com>
2024-08-14 08:18:26 -0400

    Create Basic Header and Account Card

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,25 @@
+import AccountBox from '@/components/AccountBox'
+import HeaderBox from '@/components/HeaderBox'
 import React from 'react'
 
 const Home = () => {
-	return <div>Home</div>
+	const loggedIn = { firstName: 'Jane' }
+
+	return (
+		<section className="home">
+			<div className="home-content">
+				<header className="home-header">
+					<HeaderBox
+						type="greeting"
+						title="Good Morning,"
+						user={loggedIn?.firstName || 'Guest'}
+						subtext=""
+					/>
+					<AccountBox accounts={[]} totalBanks={1} totalBalance={3469.52} />
+				</header>
+			</div>
+		</section>
+	)
 }
 
 export default Home
---
+++ components/AccountBox.tsx
@@ -0,0 +1,22 @@
+import { formatAmount } from '@/lib/utils'
+import React from 'react'
+
+interface AccountBoxProps extends TotalBalanceBoxProps {}
+
+const AccountBox = ({
+	accounts = [],
+	totalBanks,
+	totalBalance,
+}: AccountBoxProps) => {
+	return (
+		<section className="total-balance">
+			{/* <p>{user}</p> */}
+			<p>Jane Smith</p>
+			<p>{accounts[0]?.officialName || 'Bank Institution'}</p>
+			<p>{accounts[0]?.mask || 'xxxx xxxx xxxx 0000'}</p>
+			<p>{formatAmount(totalBalance)}</p>
+		</section>
+	)
+}
+
+export default AccountBox
