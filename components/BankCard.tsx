a6b781dd4876bba44460c4216ff47467efda8204
Epris Richardson <33326194+eprisr@users.noreply.github.com>
2024-08-25 07:35:44 -0400

    Redesign Bank Card

--- components/BankCard.tsx
+++ components/BankCard.tsx
@@ -2,6 +2,7 @@ import { formatAmount } from '@/lib/utils'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import { BiLogoVisa } from 'react-icons/bi'
 
 const BankCard = ({
 	account,
@@ -10,53 +11,40 @@ const BankCard = ({
 }: CreditCardProps) => {
 	return (
 		<div className="flex flex-col">
-			<Link href="/" className="bank-card">
+			<Link href="/" className="bank-card relative overflow-hidden">
 				<div className="bank-card_content">
 					<div>
-						<h1 className="text-16 font-semibold text-white">
-							{account?.name || userName}
-						</h1>
-						<p className="font-black text-white">
-							{formatAmount(account.currentBalance)}
-						</p>
+						<h1 className="text-20 text-white">{userName}</h1>
 					</div>
 
 					<article className="flex flex-col gap-2">
 						<div className="flex justify-between">
-							<h1 className="text-12 font-semibold text-white">{userName}</h1>
-							<h2 className="text-12 font-semibold text-white">
+							<h1 className="text-12 font-semibold text-white">
+								{account?.name || ''}
+							</h1>
+							<h2 className="text-10 font-semibold text-white">
 								&#9679;&#9679; / &#9679;&#9679;
 							</h2>
 						</div>
-						<p className="text-14 font-semibold tracking-[1.1px] text-white">
+						<p className="text-10 tracking-[1.1px] text-white">
 							&#9679;&#9679;&#9679;&#9679; &#9679;&#9679;&#9679;&#9679;
 							&#9679;&#9679;&#9679;&#9679;{' '}
 							<span className="text-16">{account?.mask}</span>
 						</p>
+						{showBalance && (
+							<p className="text-20 font-semibold text-white">
+								{formatAmount(account.currentBalance)}
+							</p>
+						)}
 					</article>
 				</div>
 
 				<div className="bank-card_icon">
-					<Image src="/icons/Paypass.svg" width={20} height={24} alt="pay" />
-					<Image
-						src="/icons/Mastercard.svg"
-						width={45}
-						height={32}
-						alt="mastercard"
-						className="ml-5"
-					/>
+					<BiLogoVisa className="text-5xl text-white -mb-4" />
 				</div>
-
-				<Image
-					src="/icons/lines.png"
-					width={316}
-					height={190}
-					alt="lines"
-					className="absolute top-0 left-0"
-				/>
+				<div className="bank-card-circle_one"></div>
+				<div className="bank-card-circle_two"></div>
 			</Link>
-
-			{/* COPY */}
 		</div>
 	)
 }
