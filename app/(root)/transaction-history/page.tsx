c4454b01b8c2b74abf81ec2e7a77600986cccd80
Epris Richardson <33326194+eprisr@users.noreply.github.com>
2024-08-26 12:38:31 -0400

    Implement Basic Transactions Table

--- app/(root)/transaction-history/page.tsx
+++ app/(root)/transaction-history/page.tsx
@@ -1,7 +1,31 @@
+import RecentTransactions from '@/components/RecentTransactions'
+import { getAccount, getAccounts } from '@/lib/actions/bank.actions'
+import { getLoggedInUser } from '@/lib/actions/user.actions'
 import React from 'react'
 
-const TransactionHistory = () => {
-	return <div>TransactionHistory</div>
+const TransactionHistory = async ({
+	searchParams: { id, page },
+}: SearchParamProps) => {
+	const currentPage = Number(page as string) || 1
+	const loggedIn = await getLoggedInUser()
+	const accounts = await getAccounts({ userId: loggedIn.$id })
+	if (!accounts) return
+
+	const accountsData = accounts?.data
+	const appwriteItemId = (id as string) || accountsData[0]?.appwriteItemId
+
+	const account = await getAccount({ appwriteItemId })
+
+	console.log(account)
+
+	return (
+		<RecentTransactions
+			accounts={accountsData}
+			transactions={account?.transactions}
+			appwriteItemId={appwriteItemId}
+			page={currentPage}
+		/>
+	)
 }
 
 export default TransactionHistory
