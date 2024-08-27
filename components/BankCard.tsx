6e1c1131260014d478b53ba026b53a8778906f25
Epris Richardson <33326194+eprisr@users.noreply.github.com>
2024-08-27 15:12:41 -0400

    Clean up Bank Card Styling

--- components/BankCard.tsx
+++ components/BankCard.tsx
@@ -24,20 +24,20 @@ const BankCard = ({
 
 					<article className="flex flex-col gap-2">
 						<div className="flex justify-between">
-							<h1 className="text-12 font-semibold text-white">
+							<h1 className="text-12 font-medium text-white">
 								{account?.name || ''}
 							</h1>
-							<h2 className="text-10 font-semibold text-white">
-								&#9679;&#9679; / &#9679;&#9679;
+							<h2 className="text-[8px] font-semibold text-white">
+								&#9679; &#9679; / &#9679; &#9679;
 							</h2>
 						</div>
-						<p className="text-10 tracking-[1.1px] text-white">
+						<p className="text-[8px] tracking-[3px] text-white">
 							&#9679;&#9679;&#9679;&#9679; &#9679;&#9679;&#9679;&#9679;
 							&#9679;&#9679;&#9679;&#9679;{' '}
-							<span className="text-16">{account?.mask}</span>
+							<span className="text-14 tracking-normal">{account?.mask}</span>
 						</p>
 						{stackNumber === 1 && (
-							<p className="text-20 font-semibold text-white">
+							<p className="text-20 tracking-wider font-semibold text-white">
 								{formatAmount(account.currentBalance)}
 							</p>
 						)}
