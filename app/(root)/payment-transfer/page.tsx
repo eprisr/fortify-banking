9ef01fd76eab10ebcebeeb6fc3b753d4cd368794
Epris Richardson <33326194+eprisr@users.noreply.github.com>
2024-08-27 05:05:18 -0400

    Implement Transfer Functionality

--- app/(root)/payment-transfer/page.tsx
+++ app/(root)/payment-transfer/page.tsx
@@ -1,7 +1,23 @@
+import PaymentTransferForm from '@/components/PaymentTransferForm'
+import { getAccounts } from '@/lib/actions/bank.actions'
+import { getLoggedInUser } from '@/lib/actions/user.actions'
 import React from 'react'
 
-const Transfer = () => {
-	return <div>Transfer</div>
+const Transfer = async () => {
+	const loggedIn = await getLoggedInUser()
+	const accounts = await getAccounts({ userId: loggedIn.$id })
+
+	if (!accounts) return
+
+	const accountsData = accounts?.data
+
+	return (
+		<section className="payment-transfer">
+			<section className="size-full pt-5">
+				<PaymentTransferForm accounts={accountsData} />
+			</section>
+		</section>
+	)
 }
 
 export default Transfer
