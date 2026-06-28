c730019c126d399df6ab0c1bd2e0ad299c19d82a
eprisr <eharrisburnett@gmail.com>
2026-06-28 18:42:57 -0400

    Update Navbar Title

--- app/(auth)/confirmation/page.tsx
+++ app/(auth)/confirmation/page.tsx
@@ -6,7 +6,7 @@ const Conf = async () => {
 	await connection()
 	return (
 		<>
-			<Navbar type="sub" pageTitle="Forgot password" />
+			<Navbar type="sub" pageTitle="Welcome" />
 			<section className="flex-center size-full max-sm:px-6 bg-white">
 				<Confirmation />
 			</section>
