9625a6c8696ce70d50c169c9428285ff9156dae6
eprisr <eharrisburnett@gmail.com>
2026-07-03 17:27:23 -0400

    Update Main Content Padding

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -8,7 +8,7 @@ const SignIn = async () => {
 	return (
 		<>
 			<Navbar type="sub" pageTitle="Sign in" background />
-			<section className="flex-center size-full max-sm:px-6 bg-white rounded-t-3xl">
+			<section className="flex-center size-full px-6 bg-white rounded-t-3xl">
 				<AuthForm type="signin" />
 			</section>
 		</>
