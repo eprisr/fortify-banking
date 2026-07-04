b26155e64f88f89d589d4971d4b2f5e3c3a2ed0a
eprisr <eharrisburnett@gmail.com>
2026-07-04 08:39:26 -0400

    Update Recent Transactions

--- components/RecentTransactions.tsx
+++ components/RecentTransactions.tsx
@@ -9,7 +9,7 @@ import {
 	ItemTitle,
 } from '@/components/ui/item'
 import { Card, CardContent } from './ui/card'
-import { ArrowRight } from 'lucide-react'
+import { ArrowRight, DollarSign } from 'lucide-react'
 
 export const RecentTransactions = ({ transactions }: TransactionTableProps) => {
 	const getRecentTransactions = [...transactions]
@@ -24,27 +24,37 @@ export const RecentTransactions = ({ transactions }: TransactionTableProps) => {
 					See All <ArrowRight size={12} className="inline" />
 				</p>
 			</div>
-			<Card className="py-0">
+			<Card className="py-0 rounded-lg shadow-card ring-0">
 				<CardContent className="px-0">
 					<ItemGroup className="gap-4">
 						{getRecentTransactions.map((t) => (
 							<Item key={t.id} size="sm" asChild role="listitem">
 								<a href="#">
-									<ItemMedia variant="image">
-										<Image
-											src={t.image}
-											alt={t.name}
-											width={32}
-											height={32}
-											className="object-cover grayscale"
-										/>
-									</ItemMedia>
+									{t.image ? (
+										<ItemMedia variant="image">
+											<Image
+												src={t.image}
+												alt={t.name}
+												width={32}
+												height={32}
+												className="object-cover rounded-md"
+											/>
+										</ItemMedia>
+									) : (
+										<div className="h-8 w-8 rounded-md bg-primary-700 text-white flex items-center justify-center">
+											<DollarSign size={16} strokeWidth={3} />
+										</div>
+									)}
 									<ItemContent>
 										<ItemTitle className="line-clamp-1">{t.name} - </ItemTitle>
 										<ItemDescription>{t.date}</ItemDescription>
 									</ItemContent>
 									<ItemContent className="flex-none text-center">
-										<ItemDescription>{t.amount}</ItemDescription>
+										<ItemDescription
+											className={`${t.type === 'credit' ? 'text-primary' : 'text-fortify-rose'}`}>
+											<span>{t.type === 'credit' ? '+' : '-'}$</span>
+											{Math.abs(t.amount).toFixed(2)}
+										</ItemDescription>
 									</ItemContent>
 								</a>
 							</Item>
