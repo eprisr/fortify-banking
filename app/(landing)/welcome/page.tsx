e65042181dca828c9c1a525d21049a99797145ef
eprisr <eharrisburnett@gmail.com>
2026-08-18 11:03:02 -0400

    Minimal NavBar Layout Styling

--- app/(landing)/welcome/page.tsx
+++ app/(landing)/welcome/page.tsx
@@ -1,8 +1,19 @@
+import { Button } from '@/components/ui/button'
+
 const Landing = async () => {
 	return (
 		<div>
-			<div>
-				<p>Header</p>
+			<div className="flex justify-between px-10 py-4">
+				<p>Logo</p>
+				<ul className="flex gap-7">
+					<li>Features</li>
+					<li>How it works</li>
+					<li>Security</li>
+				</ul>
+				<div>
+					<Button variant="ghost">Sign in</Button>
+					<Button variant="default">Get early access</Button>
+				</div>
 			</div>
 			<div>
 				<p>Hero</p>
