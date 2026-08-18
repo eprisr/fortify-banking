211139693761caa21877dcc495ace7b6cf974360
eprisr <eharrisburnett@gmail.com>
2026-08-18 10:57:20 -0400

    Add Basic Sections

--- app/(landing)/welcome/page.tsx
+++ app/(landing)/welcome/page.tsx
@@ -1,8 +1,31 @@
 const Landing = async () => {
 	return (
-		<>
-			<p>Landing Page</p>
-		</>
+		<div>
+			<div>
+				<p>Header</p>
+			</div>
+			<div>
+				<p>Hero</p>
+			</div>
+			<div>
+				<p>Marquee</p>
+			</div>
+			<div>
+				<p>Features</p>
+			</div>
+			<div>
+				<p>How It Works</p>
+			</div>
+			<div>
+				<p>Security</p>
+			</div>
+			<div>
+				<p>CTA</p>
+			</div>
+			<div>
+				<p>Footer</p>
+			</div>
+		</div>
 	)
 }
 
