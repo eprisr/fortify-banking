acf4b4a1a04fbb52a1b6c26b44e1e5787de5baeb
eprisr <eharrisburnett@gmail.com>
2026-08-23 12:13:14 -0400

    Update Header Box Theming

--- components/HeaderBox.tsx
+++ components/HeaderBox.tsx
@@ -1,5 +1,6 @@
 import { getLoggedInUser } from '@/lib/actions/user.actions'
 import React from 'react'
+import { cn } from '@/lib/utils'
 
 const HeaderBox = ({
 	type = 'title',
@@ -9,10 +10,16 @@ const HeaderBox = ({
 }: HeaderBoxProps) => {
 	return (
 		<div className="header-box">
-			<h1 className="header-box-title">
+			<h1
+				className={cn(
+					'header-box-title',
+					type === 'greeting' && 'font-ibmPlexSerif italic font-normal',
+				)}>
 				{title} <br />
 				{type === 'greeting' && user !== 'Guest' && (
-					<span className="text-18 text-white font-semibold">{user}!</span>
+					<span className="text-18 text-primary-foreground font-bold">
+						{user}!
+					</span>
 				)}
 			</h1>
 			<p className="header-box-subtext">{subtext}</p>
