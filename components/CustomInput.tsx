e985a56e60606d6edcac0641bd6aada04cb6d13d
eprisr <eharrisburnett@gmail.com>
2026-08-24 07:35:19 -0400

    Fix Input Focus Style and Font Size

--- components/CustomInput.tsx
+++ components/CustomInput.tsx
@@ -36,7 +36,7 @@ function CustomInput<T extends FieldValues>({
 							{required && <sup>*</sup>}
 						</FormLabel>
 					</VisuallyHiddenPrimative.Root>
-					<div className="flex w-full flex-col bg-cloud rounded-lg px-2">
+					<div className="flex w-full flex-col bg-cloud rounded-lg">
 						<FormControl>
 							<Input
 								placeholder={placeholder}
@@ -46,7 +46,7 @@ function CustomInput<T extends FieldValues>({
 										: 'text'
 								}
 								id={name}
-								className="h-14 border-none placeholder:text-base placeholder:text-ink/40"
+								className="h-14 px-5 border-none text-base! placeholder:text-base placeholder:text-ink/40"
 								{...field}
 							/>
 						</FormControl>
