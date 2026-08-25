e811d3dea51cb0eacdd9a1d8b681645f6df48be4
eprisr <eharrisburnett@gmail.com>
2026-08-24 20:47:56 -0400

    Fix Marquee Contrast

--- components/Marquee.tsx
+++ components/Marquee.tsx
@@ -6,13 +6,13 @@ interface Props {
 
 const Marquee = ({ items }: Props) => {
 	return (
-		<div className="h-12 py-4 bg-gray-100 border border-gray-300 overflow-hidden">
+		<div className="h-12 py-4 bg-cloud border-[0.5] border-gold-decorative overflow-hidden">
 			<motion.ul
 				animate={{ x: ['0%', '-50%'] }}
 				transition={{ duration: 30, ease: 'linear', repeat: Infinity }}
 				className="flex gap-16 items-center text-xs">
 				{items.map((item, i) => (
-					<li key={i} className="whitespace-nowrap">
+					<li key={i} className="text-ink whitespace-nowrap">
 						{item}
 					</li>
 				))}
