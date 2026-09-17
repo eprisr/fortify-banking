0d74f3bbd1c444cba036f1b3412b913bf9b8f7e5
eprisr <eharrisburnett@gmail.com>
2026-09-17 17:32:41 -0400

    Updated Transfer Header

--- app/(root)/(no-nav)/payment-transfer/page.tsx
+++ app/(root)/(no-nav)/payment-transfer/page.tsx
@@ -1,4 +1,4 @@
-import Navbar from '@/components/Navbar'
+import HeaderBox from '@/components/shared/HeaderBox'
 import PaymentTransferForm from '@/components/PaymentTransferForm'
 import { getAccounts } from '@/lib/actions/bank.actions'
 import { getLoggedInUser } from '@/lib/actions/user.actions'
@@ -12,18 +12,16 @@ const Transfer = async () => {
 	const accountsData = accounts?.data
 
 	return (
-		<>
-			<Navbar type="sub" pageTitle="Transfer" user={loggedIn} />
-			<section className="size-full">
-				<Suspense>
-					<PaymentTransferForm
-						accounts={accountsData}
-						currentUserEmail={loggedIn?.email}
-						isDemo={isDemoUserId(loggedIn?.$id)}
-					/>
-				</Suspense>
-			</section>
-		</>
+		<section className="size-full">
+			<HeaderBox title="Transfer" subtext="" />
+			<Suspense>
+				<PaymentTransferForm
+					accounts={accountsData}
+					currentUserEmail={loggedIn?.email}
+					isDemo={isDemoUserId(loggedIn?.$id)}
+				/>
+			</Suspense>
+		</section>
 	)
 }
 
